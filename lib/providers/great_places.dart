82129aee5f736b760924af6e8109fa3f9eff4810
Yagoo23 <yagocg7@gmail.com>
2022-09-27 14:27:38 +0200

    first commit

---
+++ lib/providers/great_places.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+import '../models/place.dart';
+
+class GreatPlaces with ChangeNotifier {
+  List<Place> _items = [];
+
+  List<Place> get items {
+    return [..._items];
+  }
+
+  void addPlace(
+    String pickTitle,
+    File pickedImage,
+  ) {
+    final newPlace = Place(
+      id: DateTime.now().toString(),
+      image: pickedImage,
+      title: pickTitle,
+      location: null,
+    );
+    _items.add(newPlace);
+    notifyListeners();
+  }
+}
