ed353948f84aaec134d29c4e44d00bd8423d9a10
Yagoo23 <yagocg7@gmail.com>
2022-09-29 13:57:28 +0200

    Project end

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
+import '../helpers/location_helper.dart';
+import '../screens/map_screen.dart';
+
 class LocationInput extends StatefulWidget {
+  final Function onSelectPlace;
+
+  LocationInput(this.onSelectPlace);
+
   @override
   _LocationInputState createState() => _LocationInputState();
 }
@@ -9,10 +17,49 @@ class LocationInput extends StatefulWidget {
 class _LocationInputState extends State<LocationInput> {
   String _previewImageUrl;
 
+  void _showPreview(double lat, double lng) {
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+      latitude: lat,
+      longitude: lng,
+    );
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
+
   Future<void> _getCurrentUserLocation() async {
-    final locData = await Location().getLocation();
-    print(locData.latitude);
-    print(locData.longitude);
+    try {
+      final locData = await Location().getLocation();
+      _showPreview(
+        locData.latitude,
+        locData.longitude,
+      );
+      widget.onSelectPlace(
+        locData.latitude,
+        locData.longitude,
+      );
+    } catch (error) {
+      return;
+    }
+  }
+
+  Future<void> _selecOnMap() async {
+    final selectedLocation = await Navigator.of(context).push<LatLng>(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (ctx) => MapScreen(
+          isSelecting: true,
+        ),
+      ),
+    );
+    if (selectedLocation == null) {
+      return;
+    }
+    _showPreview(selectedLocation.latitude, selectedLocation.longitude);
+    widget.onSelectPlace(
+      selectedLocation.latitude,
+      selectedLocation.longitude,
+    );
   }
 
   @override
@@ -56,7 +103,7 @@ class _LocationInputState extends State<LocationInput> {
               style: TextButton.styleFrom(
                 primary: Theme.of(context).primaryColor,
               ),
-              onPressed: () {},
+              onPressed: _selecOnMap,
             ),
           ],
         ),
